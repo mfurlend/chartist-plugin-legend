8cf90890f85df5d3c8c85dd876f361d76b618f22
Mike Furlender <mfurlend@gmail.com>
2016-06-10 12:40:39 -0400

    modified to allow multiple instances

--- chartist-plugin-legend.js
+++ chartist-plugin-legend.js
@@ -1,147 +1,132 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['chartist'], function (chartist) {
-            return (root.returnExportsGlobal = factory(chartist));
+        define(['chartist', 'jquery'], function (chartist, jquery) {
+            return (root.returnExportsGlobal = factory(chartist, jquery));
         });
     } else if (typeof exports === 'object') {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like enviroments that support module.exports,
         // like Node.
-        module.exports = factory(require('chartist'));
+        module.exports = factory(require('chartist'), require('jquery'));
     } else {
-        root['Chartist.plugins.legend'] = factory(root.Chartist);
+        root['Chartist.plugins.legend'] = factory(root.chartist, root.jquery);
     }
-}(this, function (Chartist) {
+}(this, function () {
+
     /**
      * This Chartist plugin creates a legend to show next to the chart.
      *
      */
-    'use strict';
+    (function (Chartist, $) {
+        'use strict';
 
-    var defaultOptions = {
-        className: '',
-        legendNames: false,
-        clickable: true,
-        onClick: null
-    };
 
-    Chartist.plugins = Chartist.plugins || {};
+        var defaultOptions = {
+            className: '',
+            legendNames: false,
+            clickable: true
+        };
 
-    Chartist.plugins.legend = function (options) {
+        Chartist.plugins = Chartist.plugins || {};
 
-        options = Chartist.extend({}, defaultOptions, options);
+        Chartist.plugins.legend = function (options) {
 
-        return function legend(chart) {
-            var existingLegendElement = chart.container.querySelector('.ct-legend');
-            if (existingLegendElement) {
-                // Clear legend if already existing.
-                existingLegendElement.parentNode.removeChild(existingLegendElement);
-            }
+            options = Chartist.extend({}, defaultOptions, options);
 
-            // Set a unique className for each series so that when a series is removed,
-            // the other series still have the same color.
-            if (options.clickable) {
-                var newSeries = chart.data.series.map(function (series, seriesIndex) {
-                    if (typeof series !== 'object') {
-                        series = {
-                            value: series
-                        };
-                    }
+            return function legend(chart) {
 
-                    series.className = series.className || chart.options.classNames.series + '-' + Chartist.alphaNumerate(seriesIndex);
-                    return series;
-                });
-                chart.data.series = newSeries;
-            }
+                var $chart = $(chart.container),
+                    legendClass = chart instanceof Chartist.Pie ? 'ct-legend-inside' : '';
 
-            var legendElement = document.createElement('ul'),
-                isPieChart = chart instanceof Chartist.Pie;
-            legendElement.className = 'ct-legend';
-            if (chart instanceof Chartist.Pie) {
-                legendElement.classList.add('ct-legend-inside');
-            }
-            if (typeof options.className === 'string' && options.className.length > 0) {
-                legendElement.classList.add(options.className);
-            }
-
-            var removedSeries = [],
-                originalSeries = chart.data.series.slice(0);
-
-            // Get the right array to use for generating the legend.
-            var legendNames = chart.data.series,
-                useLabels = isPieChart && chart.data.labels;
-            if (useLabels) {
-                var originalLabels = chart.data.labels.slice(0);
-                legendNames = chart.data.labels;
-            }
-            legendNames = options.legendNames || legendNames;
-
-            // Loop through all legends to set each name in a list item.
-            legendNames.forEach(function (legend, i) {
-                var li = document.createElement('li');
-                li.className = 'ct-series-' + i;
-                li.setAttribute('data-legend', i);
-                li.textContent = legend.name || legend;
-                legendElement.appendChild(li);
-            });
-            chart.container.appendChild(legendElement);
-
-            if (options.clickable) {
-                legendElement.addEventListener('click', function (e) {
-                    var li = e.target;
-                    if (li.parentNode !== legendElement || !li.hasAttribute('data-legend'))
-                        return;
-                    e.preventDefault();
-
-                    var seriesIndex = parseInt(li.getAttribute('data-legend')),
-                        removedSeriesIndex = removedSeries.indexOf(seriesIndex);
-
-                    if (removedSeriesIndex > -1) {
-                        // Add to series again.
-                        removedSeries.splice(removedSeriesIndex, 1);
-                        li.classList.remove('inactive');
-                    } else {
-                        // Remove from series, only if a minimum of one series is still visible.
-                        if (chart.data.series.length > 1) {
-                            removedSeries.push(seriesIndex);
-                            li.classList.add('inactive');
+                var $legend = $chart.data('legend');
+                // Set a unique className for each series so that when a series is removed,
+                // the other series still have the same color.
+                if (options.clickable) {
+                    chart.data.series.forEach(function (series, seriesIndex) {
+                        if (typeof series !== 'object') {
+                            series = {
+                                data: series
+                            };
                         }
-                    }
 
-                    // Reset the series to original and remove each series that
-                    // is still removed again, to remain index order.
-                    var seriesCopy = originalSeries.slice(0);
-                    if (useLabels) {
-                        var labelsCopy = originalLabels.slice(0);
-                    }
-
-                    // Reverse sort the removedSeries to prevent removing the wrong index.
-                    removedSeries.sort().reverse();
-
-                    removedSeries.forEach(function (series) {
-                        seriesCopy.splice(series, 1);
-                        if (useLabels) {
-                            labelsCopy.splice(series, 1);
-                        }
+                        series.className = series.className || chart.options.classNames.series + '-' + Chartist.alphaNumerate(seriesIndex);
                     });
+                }
 
-                    if (options.onClick) {
-                        options.onClick(chart, e);
-                    }
+                if ($legend) {
+                    $legend.remove();
+                }
 
-                    chart.data.series = seriesCopy;
-                    if (useLabels) {
-                        chart.data.labels = labelsCopy;
-                    }
+                $legend = $chart
+                    .append('<ul class="ct-legend"></ul>')
+                    .find('.ct-legend')
+                    .addClass(legendClass)
+                    .addClass(options.className);
 
-                    chart.update();
+                var insertLegendItem = function (i, name) {
+                    $legend.append('<li class="ct-series-' + i + '" data-legend="' + i + '">' + name + '</li>');
+                };
+
+                var removedSeries = [],
+                    originalSeries = chart.data.series.slice(0);
+
+                if (options.clickable) {
+                    $legend.on('click', 'li', function (e) {
+                        e.preventDefault();
+
+                        var seriesIndex = parseInt($(this).attr('data-legend')),
+                            removedSeriesIndex = removedSeries.indexOf(seriesIndex);
+
+                        if (removedSeriesIndex > -1) {
+                            // Add to series again.
+                            removedSeries.splice(removedSeriesIndex, 1);
+
+                            $(this).removeClass('inactive');
+                        } else {
+                            // Remove from series, only if a minimum of one series is still visible.
+                            if (chart.data.series.length > 1) {
+                                removedSeries.push(seriesIndex);
+
+                                $(this).addClass('inactive');
+                            }
+                        }
+
+                        // Reset the series to original and remove each series that
+                        // is still removed again, to remain index order.
+                        var seriesCopy = originalSeries.slice(0);
+
+                        // Reverse sort the removedSeries to prevent removing the wrong index.
+                        removedSeries.sort().reverse();
+
+                        removedSeries.forEach(function (series) {
+                            seriesCopy.splice(series, 1);
+                        });
+
+                        chart.data.series = seriesCopy;
+
+                        chart.update();
+                    });
+                }
+
+                // Get the right array to use for generating the legend.
+                var legendNames = chart.data.series;
+                if (chart instanceof Chartist.Pie) {
+                    legendNames = chart.data.labels;
+                }
+                legendNames = options.legendNames || legendNames;
+
+                // Loop through all legends to set each name in a list item.
+                legendNames.forEach(function (legend, i) {
+                    var legendName = legend.name || legend;
+
+                    insertLegendItem(i, legendName);
                 });
-            }
+            };
 
         };
 
-    };
+    }(Chartist, $));
 
     return Chartist.plugins.legend;
 
